6c18eafa6748d7357e43a6bb96693ee51606e483
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-02-11 12:01:35 -0600

    Cache static files

--- public/sw.js
+++ public/sw.js
@@ -1,28 +1,27 @@
-const CACHE_NAME = 'capshare-v1';
-// var urlsToCache = [
-//     '/',
-//     'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
-//     '/__/firebase/4.8.1/firebase-app.js',
-//     '/__/firebase/4.8.1/firebase-auth.js',
-//     '/__/firebase/4.8.1/firebase-database.js',
-//     '/__/firebase/4.8.1/firebase-messaging.js',
-//     '/__/firebase/4.8.1/firebase-storage.js',
-//     '/__/firebase/init.js',
-//     'https://code.jquery.com/jquery-3.2.1.slim.min.js',
-//     'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
-//     'https://cdn.jsdelivr.net/npm/idb-keyval@2.3.0/idb-keyval.min.js'
-// ];
-
+const APP_CACHE = 'capshare-v1';
 const IMG_CACHE = 'capshare-images';
+const urlsToCache = [
+    '/favicon.ico',
+    '/__/firebase/4.9.1/firebase-app.js',
+    '/__/firebase/4.9.1/firebase-auth.js',
+    '/__/firebase/4.9.1/firebase-database.js',
+    '/__/firebase/4.9.1/firebase-storage.js',
+    '/__/firebase/init.js',
+    '/static/css/bootstrap.min.css',
+    'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    '/static/js/jquery.min.js',
+    '/static/js/bootstrap.min.js',
+    '/static/js/idb-keyval.min.js'
+];
 
 // Install handler
 self.addEventListener('install', function (event) {
     // Perform install steps
-    // event.waitUntil(
-    //     caches.open(CACHE_NAME).then(function (cache) {
-    //         return cache.addAll(urlsToCache);
-    //     })
-    // );
+    event.waitUntil(
+        caches.open(APP_CACHE).then(function (cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
 // Fetch handler
@@ -37,6 +36,14 @@ self.addEventListener('fetch', function (event) {
                         cache.put(event.request, response.clone());
                         return response;
                     });
+                })
+            })
+        );
+    } else {
+        event.respondWith(
+            caches.open(APP_CACHE).then(function (cache) {
+                return cache.match(event.request).then(function (resp) {
+                    return resp || fetch(event.request);
                 })
             })
         );
